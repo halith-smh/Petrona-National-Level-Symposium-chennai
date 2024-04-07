2b44ba2106bfed9e2096ef6ef9b69a69f415c479
Mohamed Halith <mohamedhalith.smh@gmail.com>
2024-04-08 00:33:28 +0530

    optimized Particles

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -1,38 +1,35 @@
-import React, { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 
-import ParticleConfig from '../utils/particles.json';
+import ParticleConfig from '../utils/particles.json'
 
 import { loadFull } from "tsparticles";
-
-function Particle() {
+export default function Particle() {
   const [init, setInit] = useState(false);
-
   useEffect(() => {
-    async function initParticleEngine() {
-      try {
-        const engine = await initParticlesEngine();
-        await loadFull(engine);
-        setInit(true);
-      } catch (error) {
-        console.error("Error initializing particles:", error);
-      }
-    }
-
-    initParticleEngine();
+    console.log("init");
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
+
+  const particlesLoaded = (container) => {
+  };
 
   return (
     <>
       {init && (
         <Particles
           id="tsparticles"
-          style={{ zIndex: 1 }}
+          particlesLoaded={particlesLoaded}
+          style={{
+            zIndex: 1,
+          }}
           options={ParticleConfig}
         />
       )}
     </>
   );
 }
-
-export default Particle;
